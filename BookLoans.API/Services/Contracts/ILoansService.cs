249c62983dd4e0fde2ca295a2c0be05623ab5a51
Fábio Pereira <fabiopereira.fps@gmail.com>
2021-06-25 02:50:05 -0300

    Update service

---
+++ BookLoans.API/Services/Contracts/ILoansService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using BookLoans.API.Models;
+using BookLoans.Domain.Entities;
+
+namespace BookLoans.API.Services.Contracts
+{
+    public interface ILoansService
+    {
+        IEnumerable<Loan> GetAll();
+        void LoanBook(LoanBookRequest request);
+    }
+}
