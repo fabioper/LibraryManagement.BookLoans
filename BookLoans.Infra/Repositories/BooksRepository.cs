6f2926732225da81c49bcdd62199dfff94041e48
Fábio Pereira <fabiopereira.fps@gmail.com>
2021-06-25 02:14:05 -0300

    Configure EF Core context and create repositories

---
+++ BookLoans.Infra/Repositories/BooksRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using BookLoans.Domain.Entities;
+using BookLoans.Domain.Interfaces;
+
+namespace BookLoans.Infra.Repositories
+{
+    public class BooksRepository : IBooksRepository
+    {
+        public IEnumerable<Book> Add(Book book)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Book GetById(int bookId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BookLoans.Infra/Repositories/LoansRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using BookLoans.Domain.Entities;
+using BookLoans.Domain.Interfaces;
+
+namespace BookLoans.Infra.Repositories
+{
+    public class LoansRepository : ILoansRepository
+    {
+        public IEnumerable<Loan> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Add(Loan loan)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(Loan loan)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
